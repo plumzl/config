dfb2c72752c971de2d62448c3f9c353af7fff9fc
plumzl <macheala@gmail.com>
2012-12-01 22:31:01 -0500

    Added git branch to bash prompt.

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,14 +1,22 @@
-# echo -e '\033[36mhell\033[m'
 function __prompt_jobs() {
-    # local procs="$(jobs | awk \'""{ if \(\$2 ~ /"$1"/\) print \$3}\'"")"
-    local procs="$(jobs | awk "{ if (\$2 ~ /$1/) print \$3 }")"
+    local procs="$(jobs | awk "{ if (\$2 ~ /$1/) print \$3}")"
     echo $procs
 }
 
+function __prompt_git() {
+    branch="$(git symbolic-ref HEAD 2>/dev/null)" || branch="" 
+    branch=${branch##refs/heads/}
+    if [ -n "$branch" ]; then
+        branch=[$branch]
+    fi
+    echo $branch
+}
+
 function __set_prompt() {
+    local branch="\[\e[33m\]\$(__prompt_git)"
     local job_info="\[\e[31m\]\$(__prompt_jobs Stopped) "
     job_info="$job_info\[\e[32m\]\$(__prompt_jobs Running)"
-    export PS1="\[\e[32m\]\h: \[\e[38;5;8m\]\w/ $job_info\n\[\e[38;5;15m\]\$\[\e[38;5;7m\] "
+    export PS1="\[\e[32m\]\h: \[\e[38;5;8m\]\w/ $branch $job_info\n\[\e[38;5;15m\]\$\[\e[38;5;7m\] "
 }
 
 __set_prompt
